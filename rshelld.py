a3a51345211d495845d6a2dac8c3d0cbfd4e42e0
YiTong Zhao <63088368+origamizyt@users.noreply.github.com>
2021-02-18 11:26:36 +0800

    Read port from configuration first

--- rshelld.py
+++ rshelld.py
@@ -1,14 +1,17 @@
 from server import ShellServer
+from config import default_config
 import sys
+
+config = default_config()
 
 try:
     port = sys.argv[1]
 except IndexError:
-    port = input('Enter port: ')
+    port = config.get('server.port') or input('Enter port: ')
 
 port = int(port)
 s = ShellServer(port)
-print('Server running on localhost:%i' % port)
+print('Server running on 0.0.0.0:%i' % port)
 try:
     s.serve()
 except KeyboardInterrupt:
